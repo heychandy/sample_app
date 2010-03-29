ddf93c5bc5cec7c661e127501f29c92bc3176494
Chandy Chinnasamy <heychandy@gmail.com>
2010-03-29 11:15:16 -0500

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   map.root :controller => 'pages', :action => 'home'
+  map.resources :users
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about '/about', :controller => 'pages', :action => 'about'
   map.help '/help', :controller => 'pages', :action => 'help'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,11 +16,51 @@ describe User do
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
-      :email => "value for email"
+      :email => "email@example.com"
     }
   end
 
   it "should create a new instance given valid attributes" do
     User.create!(@valid_attributes)
   end
+  
+  it "should require a name" do
+    no_name_user = User.new(@valid_attributes.merge(:name=>""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should require an email" do
+    no_email_user = User.new(@valid_attributes.merge(:email=>""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@valid_attributes.merge(:name=>long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@valid_attributes.merge(:email =>address))
+      valid_email_user.should be_valid
+    end
+  end
+  
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org first.last@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@valid_attributes.merge(:email =>address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+  
+  it "should reject duplicate email addresses" do
+    User.create!(@valid_attributes)
+    user_with_duplicate_email = User.new(@valid_attributes)
+    user_with_duplicate_email.should_not be_valid
+  end
 end
+
+
