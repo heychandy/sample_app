bee4d1fbaeb1707fbab6106aa92bb8390dfb8c6a
Chandy Chinnasamy <heychandy@gmail.com>
2010-03-31 16:30:18 -0500

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -38,12 +38,16 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.root :controller => 'pages', :action => 'home'
   map.resources :users
+  map.root :controller => 'users', :action => 'new'
+  map.resources :sessions, :only => [:new, :create, :destroy]
+  map.signin '/signin', :controller => 'sessions', :action => 'new'
+  map.signout '/signout', :controller => 'sessions', :action => 'destroy'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about '/about', :controller => 'pages', :action => 'about'
   map.help '/help', :controller => 'pages', :action => 'help'
-  map.signup '/signup', :controller => 'users', :action => 'new'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -27,4 +27,43 @@ describe "Users" do
       end
     end
   end
+  describe "remember me" do
+    before (:each) do
+      @user = Factory(:user)
+    end
+    it "should have a remember_me! method" do
+      @user.should respond_to(:remember_me!)
+    end
+    it "should have a remember token" do
+      @user.should respond_to(:remember_token)
+    end
+    it "should set the remembertoken" do
+      @user.remember_me!
+      @user.remember_token.should_not be_nil
+    end
+  end
+  describe "sign in/out" do
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email, :with=>""
+        fill_in :password, :with=>""
+        click_button
+        response.should render_template('sessions/new')
+        response.should have_tag("div.flash.error", /invalid/i)
+      end
+    end
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email, :with=> user.email
+        fill_in :password, :with=> user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
